514fb05057c5303ad9f57168c69873d938bc1a6f
KAPOOR KUMAR <137023368+kapoor2q@users.noreply.github.com>
2024-10-05 12:44:55 +0000

    commit

---
+++ 01_basic/variables_02.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) we are using nodejs,not browser
+
+console.log(3+3)
+//number => 2 to power 53
+//bigint
+//String=>""
+//blooleam=>true/false
+//null=> standalone value
+//undefined=>
+//symbol => unique
+///object
+
+console.log(typeof undefined)//undefine
+console.log(typeof null)//object
